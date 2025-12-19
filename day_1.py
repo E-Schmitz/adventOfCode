d7ac665be5ef77fb200438d62d0cfd1d3430b9aa
E-Schmitz <enzo.schmitz99@gmail.com>
2025-12-19 22:09:35 +0100

    Removed unnecessary function

--- day_1.py
+++ day_1.py
@@ -5,15 +5,8 @@ with open('day_1_rotations', 'r') as raw:
     rotation_list = raw.read().splitlines()
 
 
-def performRotation(start_idx, rotation, dial):
+def calculateIdx(start_idx, rotation, dial):
     direction, amount = rotation [0], int(rotation[1:])
-
-    current_idx = calculateIdx(start_idx, direction, amount, dial)
-
-    return current_idx
-
-
-def calculateIdx(start_idx, direction, amount, dial):
     temp = start_idx
 
     if direction == "L":
@@ -40,7 +33,7 @@ def obtainPasscode(_rotations):
     counter : int = 0
 
     for rot in _rotations:
-        current_idx = performRotation(current_idx, rot, dial)
+        current_idx = calculateIdx(current_idx, rot, dial)
         if current_idx == 0:
             counter += 1
 
