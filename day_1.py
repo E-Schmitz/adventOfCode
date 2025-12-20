5960753b34bfc99f1e206b9a73683d93b8ebcf41
E-Schmitz <enzo.schmitz99@gmail.com>
2025-12-20 14:16:39 +0100

    Added solution for part 2 in python

--- day_1.py
+++ day_1.py
@@ -4,40 +4,47 @@ import numpy as np
 with open('day_1_rotations', 'r') as raw:
     rotation_list = raw.read().splitlines()
 
+test_rotations = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
 
 def calculateIdx(start_idx, rotation, dial):
     direction, amount = rotation [0], int(rotation[1:])
-    temp = start_idx
+    count = amount // dial
+    rest = amount % dial
 
     if direction == "L":
         # Modulo ensures no runtime overhead for large inputs
-        temp = (temp -amount) % len(dial)
-        return temp
+        idx = (start_idx - rest) % dial
+        # Avoid edge cases where landing on 0 is counted twice
+        if (rest > 0 and start_idx != 0 and rest >= start_idx):
+            count +=1
+
+        return idx, count
     
     elif direction == "R":
-        temp = (temp + amount) % len(dial)
-        return temp
+        idx = (start_idx + rest) % dial
+
+        if (rest > 0 and start_idx != 0 and(start_idx + rest >= dial)):
+            count +=1
+
+        return idx, count
     else:
         print("Rotation direction is neither L or R")
 
-
-#### Summary 
-# Takes a list of rotation inputs and returns the amount of times the index pointed to 0
-####    
+ 
 def obtainPasscode(_rotations):
-    # Range set to 100, to include all int 0 to 99
-    dial = np.arange(0,100)
+    # No actual array needed, just the length
+    dial = 100
     # Starting index is given inside instructions (50)
     current_idx = 50
     # Counter for occurrences of 0
-    counter : int = 0
+    total_count : int = 0
 
     for rot in _rotations:
-        current_idx = calculateIdx(current_idx, rot, dial)
-        if current_idx == 0:
-            counter += 1
-
-    return counter
+        current_idx, temp_counter = calculateIdx(current_idx, rot, dial)
+        total_count += temp_counter
+        
+    return total_count
     
 print(obtainPasscode(rotation_list))
+#print(obtainPasscode(test_rotations))
 
